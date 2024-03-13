fbb8145167de07de3abfd6bb66d7f54d3b3ea74a
Mvoi <mvoifranklin@gmail.com>
2024-03-13 21:04:08 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/activate
+#!/usr/bin/python3
 for i in range(97, 123):
-	if i != 101 and i != 113:
-		print(f"{i}".format(character=chr(i)), end= " ")
+    if i != 101 and i != 113:
+        print(f"{i}".format(character=chr(i)), end= " ")
