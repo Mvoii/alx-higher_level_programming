23e839f3bf46b8da98fca4a31afb32949b0be2c5
Mvoi <mvoifranklin@gmail.com>
2024-03-13 21:11:43 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 for first_digit in range(10):
     for second_digit in range(10):
         if first_digit == 8 and second_digit == 9:
-            print("{one}{two}".format(one = first_digit, two = second_digit), end="\n")
+            print("{one}{two}".format(one=first_digit,
+                                      two=second_digit), end="\n")
         elif first_digit < second_digit and first_digit != second_digit:
-            print("{one}{two}".format(one = first_digit, two = second_digit), end=", ")
+            print("{one}{two}".format(one=first_digit, two=second_digit), end=", ")
