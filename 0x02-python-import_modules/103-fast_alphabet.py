4cdf13e1282b9a4262bcaed4aa20fd9e444bd163
Mvoi <mvoifranklin@gmail.com>
2024-03-14 20:22:40 +0300

    Update 103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,12 +1,3 @@
-from magic_calculation_102 import add, sub
-
-
-def magic_calculation(a, b):
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-    else:
-        return (sub(a, b))
+#!/usr/bin/python3
+from string import ascii_uppercase
+print(ascii_uppercase)
