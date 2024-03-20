b0a495f7a796b8dc9bf85aae8fe818962ba46f0b
Mvoi <mvoifranklin@gmail.com>
2024-03-20 21:46:47 +0300

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-def uniq_add(my_list=[]):
-    my_set = set(my_list)
-    number_list = list(my_set)
-    result = sum(my_set)
-    return result
+def common_elements(set_1, set_2):
+    return set_1.intersection(set_2)
